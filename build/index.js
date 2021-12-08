580908c65b9cf20da80c8b40c24fab63861bbffb
blue <blue@DESKTOP-Q1MIF3C>
2021-12-08 17:01:48 +0700

    - setup Prettier - type - interface - strickNullChecks

--- build/index.js
+++ build/index.js
@@ -2,3 +2,11 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(5, 7));
+var a = BigInt(10);
+console.log(typeof a);
+const contacts = [];
+const newContact = {
+    name: "",
+    phone: "",
+    email: "",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-function sum(a: number, b: number){
-    return a+b;
+import { send } from "./mailer";
+
+function sum(a: number, b: number) {
+  return a + b;
 }
 
-console.log(sum(5,7));
+console.log(sum(5, 7));
+
+var a: bigint = BigInt(10);
+console.log(typeof a);
+
+interface Contact {
+  name: string;
+  phone: string;
+  email?: string;
+}
+
+const contacts: Contact[] = [];
+const newContact: Contact = {
+  name: "Smith",
+  phone: "123",
+  email: "",
+};
+const newContact2: Contact = {
+  name: "222",
+  phone: "",
+};
+
+contacts.push(newContact);
+
+if (newContact.email) {
+  send(newContact.email, "1", "2");
+}
