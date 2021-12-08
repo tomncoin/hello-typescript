9f60fa5b42ae388b6c1eaefa25dd81f7924bfbe2
blue <blue@DESKTOP-Q1MIF3C>
2021-12-08 17:12:28 +0700

    optional chainning

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,20 @@ console.log(sum(5, 7));
 var a: bigint = BigInt(10);
 console.log(typeof a);
 
+interface Pet {
+  name: string;
+}
+
 interface Contact {
   name: string;
   phone: string;
   email?: string;
+  pet?: Pet;
+  address?: Address[];
+}
+
+interface Address {
+  city: string;
 }
 
 const contacts: Contact[] = [];
@@ -20,6 +30,9 @@ const newContact: Contact = {
   name: "Smith",
   phone: "123",
   email: "",
+  pet: {
+    name: "A",
+  },
 };
 const newContact2: Contact = {
   name: "222",
@@ -31,3 +44,16 @@ contacts.push(newContact);
 if (newContact.email) {
   send(newContact.email, "1", "2");
 }
+
+function getPetName(contact: Contact): string {
+  return contact.pet?.name || "";
+}
+
+function getFirstAddress(contact: Contact) {
+  return contact.address?.[0];
+}
+
+console.log(getPetName(newContact));
+console.log(getPetName(newContact2));
+
+console.log(getFirstAddress(newContact)?.city);
